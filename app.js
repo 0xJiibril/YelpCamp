590cf2ff2d4e40a074c392617e0e3199f0941877
Anas <Kadastross@users.noreply.github.com>
2021-09-20 01:05:04 +0530

    Adding CRUD#1

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const app=express();
 
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded({extended:true}));
 
 
 app.get('/',(req,res)=>{
@@ -32,13 +33,22 @@ app.get('/campgrounds',async (req,res)=>{
 app.get('/campgrounds/new',(req,res)=>{    
     res.render('campgrounds/new.ejs');
 });
+app.post('/campgrounds',async (req,res)=>{    
+    const campground=new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
 app.get('/campgrounds/:id',async (req,res)=>{
     const campground=await Campground.findById(req.params.id);
     res.render('campgrounds/show.ejs',{campground});
 });
-
+app.get('/campgrounds/:id/edit',async(req,res)=>{
+    const campground =await Campground.findById(req.params.id);
+    res.render('campgrounds/edit.ejs',{campground});
+})
 
 app.listen(5500,()=>{
     console.log('Serving  on port 5500');
+
 })
 
