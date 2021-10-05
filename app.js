b946b654d617dc33171ddbfafd5ec8c5c5a881c4
Anas <Kadastross@users.noreply.github.com>
2021-10-05 16:29:40 +0530

    Added Server-side validations
    Added server side form validation using JOI

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const mongoose = require ('mongoose');
 const path=require('path');
 const ejsMate=require('ejs-mate');
-const Joi=require('joi');
+const {campgroundSchema}=require('./schema');
 const catchAsync=require('./utils/catchAsync');
 const ExpressError=require('./utils/ExpressError');
 const methodOverride=require('method-override');
@@ -29,28 +29,28 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
+const validateCampground=(req,res,next)=>{
+     const {error}=campgroundSchema.validate(req.body);
+    if(error){
+        const msg=error.details.map(el=>el.message).join(',');
+        throw new ExpressError(msg,400);
+    }
+    else{
+        next();
+    }
+}
+
 app.get('/',(req,res)=>{
     res.render('home');
 })
-app.get('/campgrounds',catchAsync( async (req,res)=>{
+app.get('/campgrounds' ,catchAsync( async (req,res)=>{
     const campgrounds=await Campground.find({});
     res.render('campgrounds/index.ejs',{campgrounds});
 }));
 app.get('/campgrounds/new',catchAsync((req,res)=>{    
     res.render('campgrounds/new.ejs');
 }));
-app.post('/campgrounds',catchAsync(async (req,res)=>{    
-    const campgroundSchema=Joi.object({
-        campground:Joi.object().required({
-            title:Joi.string().required(),
-            price:Joi.number().required().min(0)
-        })
-    });
-    const {error}=campgroundSchema.validate(req.body);
-    if(error){
-        const msg=error.details.map(el=>el.message).join(',');
-        throw new ExpressError(msg,400);
-    }
+app.post('/campgrounds',validateCampground,catchAsync(async (req,res)=>{    
     const campground=new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
