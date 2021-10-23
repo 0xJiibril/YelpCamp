bc296328b09f4b765c4ead42ec388018b9eba42a
Anas <Kadastross@users.noreply.github.com>
2021-10-23 19:01:17 +0530

    Added Auth for "Reviews"

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { validateReview } = require('../middleware');
+const { validateReview,isLoggedIn, isReviewAuthor } = require('../middleware');
 
 const Campground = require("../models/campground");
 const Review = require("../models/review");
@@ -11,11 +11,12 @@ const ExpressError = require("../utils/ExpressError");
 const catchAsync = require("../utils/catchAsync");
 
 router.post(
-	"/",
+	"/",isLoggedIn,
 	validateReview,
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id);
 		const review = new Review(req.body.review);
+		review.author=req.user._id;
 		campground.reviews.push(review);
 		await review.save();
 		await campground.save();
@@ -25,7 +26,7 @@ router.post(
 );
 
 router.delete(
-	"/:reviewId",
+	"/:reviewId",isLoggedIn,isReviewAuthor,
 	catchAsync(async (req, res) => {
 		const { id, reviewId } = req.params;
 		Campground.findByIdAndDelete(id, { $pull: { reviews: { reviewId } } });
