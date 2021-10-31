b12a88778388fe1ffad446fa4fe11e351551989a
Anas <Kadastross@users.noreply.github.com>
2021-10-31 11:30:43 +0530

    Added Star rating system

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
-
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 const reviews = require("../controllers/reviews");
-
 const { reviewSchema } = require("../schemas.js");
-
 const ExpressError = require("../utils/ExpressError");
 const catchAsync = require("../utils/catchAsync");
 
